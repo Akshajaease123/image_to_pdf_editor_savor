536ca00fc551e36a838151a2446c6c715a520c5b
Akshaja V Maiya <82953729+Akshajaease123@users.noreply.github.com>
2023-10-08 21:50:44 +0530

    Update ImageImportApp.java
    The Sepia filter now works correctly by changing each pixel of image

--- img_edit/ImageImportApp.java
+++ img_edit/ImageImportApp.java
@@ -152,7 +152,30 @@ public class ImageImportApp extends JFrame {
                 filter = new ColorConvertOp(ColorSpace.getInstance(ColorSpace.CS_GRAY), null);
                 break;
             case "Sepia":
-                filter = createSepiaFilter();
+                BufferedImage sepiaImage = new BufferedImage(originalImage.getWidth(), originalImage.getHeight(), BufferedImage.TYPE_INT_RGB);
+
+    for (int y = 0; y < originalImage.getHeight(); y++) {
+        for (int x = 0; x < originalImage.getWidth(); x++) {
+            int rgb = originalImage.getRGB(x, y);
+            int r = (rgb >> 16) & 0xFF;
+            int g = (rgb >> 8) & 0xFF;
+            int b = rgb & 0xFF;
+
+            int tr = (int) (0.393 * r + 0.769 * g + 0.189 * b);
+            int tg = (int) (0.349 * r + 0.686 * g + 0.168 * b);
+            int tb = (int) (0.272 * r + 0.534 * g + 0.131 * b);
+
+            // Clamp values to the 0-255 range
+            tr = Math.min(255, Math.max(0, tr));
+            tg = Math.min(255, Math.max(0, tg));
+            tb = Math.min(255, Math.max(0, tb));
+
+            int sepiaRGB = (tr << 16) | (tg << 8) | tb;
+            sepiaImage.setRGB(x, y, sepiaRGB);
+        }
+    }
+
+    displayedImage = sepiaImage;
                 break;
             case "Original":
                 // No filter needed for original
